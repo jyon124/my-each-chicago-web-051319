01a40f3be212228a5ca78039e66081627b77777b
Jooyon Won <jyon1204@gmail.com>
2019-05-05 21:56:30 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,15 +2,9 @@ def my_each(words)
   counts = 0
 
   while counts < words.length
-    yield(words[i])
+  
     counts += 1
   end
   words
 end
 
-my_each(words) do
-  |words|
-    if words.length > counts
-      counts += 1
-    end
-end
